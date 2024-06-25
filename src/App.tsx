df452ffb21554d72a75ad2a34b673a64d0aee23e
Siltarae <sjkwang1@gmail.com>
2024-06-25 22:40:00 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { appContainer, board, button } from "./App.css.ts";
+import BoardList from "./components/BoardList/BoardList.tsx";
+import ListsContainer from "./components/ListsContainer/ListsContainer.tsx";
+import { useTypedSelector } from "./hooks/redux.ts";
+
+function App() {
+  const [activeBoardId, setActiveBoardId] = useState("board-0");
+
+  const boards = useTypedSelector((state) => state.boards.boardArray);
+
+  const getActiveBoard = boards.filter(
+    (board) => board.boardId === activeBoardId
+  )[0];
+
+  const lists = getActiveBoard.lists;
+
+  return (
+    <div className={appContainer}>
+      <BoardList
+        activeBoardId={activeBoardId}
+        setActiveBoardId={setActiveBoardId}
+      />
+      <div className={board}>
+        <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
+      </div>
+      <div className={button}>
+        <button>이 게시판 삭제 하기</button>
+        <button></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List/List.tsx
@@ -0,0 +1,73 @@
+import React, { FC } from "react";
+import { GrSubtract } from "react-icons/gr";
+import Task from "../Task/Task";
+import { IList, ITask } from "../../types";
+import ActionButton from "../ActionButton/ActionButton";
+import { useTypedDispatch } from "../../hooks/redux";
+import { deleteList, setModalActive } from "../../store/slices/boardsSlice";
+import { addLog } from "../../store/slices/loggerSlice";
+import { v4 } from "uuid";
+import { setModalData } from "../../store/slices/modalSlice";
+import { delteButton, header, listWrapper, name } from "./List.css";
+
+type TListProps = {
+  list: IList;
+  boardId: string;
+};
+
+const List: FC<TListProps> = ({ list, boardId }) => {
+  const dispatch = useTypedDispatch();
+
+  const handlListDelete = (listId: string) => {
+    dispatch(deleteList({ boardId, listId }));
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `리스트 삭제하기 : ${list.listName}`,
+        logAuthor: "User",
+        logTimestamp: String(Date.now()),
+      })
+    );
+  };
+
+  const handleTaskChange = (
+    boardId: string,
+    listId: string,
+    taskId: string,
+    task: ITask
+  ) => {
+    dispatch(setModalData({ boardId, listId, task }));
+    dispatch(setModalActive(true));
+  };
+
+  return (
+    <div className={listWrapper}>
+      <div className={header}>
+        <div className={name}>{list.listName}</div>
+        <GrSubtract
+          className={delteButton}
+          onClick={() => handlListDelete(list.listId)}
+        />
+      </div>
+      {list.tasks.map((task, index) => (
+        <div
+          onClick={() =>
+            handleTaskChange(boardId, list.listId, task.taskId, task)
+          }
+          key={task.taskId}
+        >
+          <Task
+            taskName={task.taskName}
+            taskDescription={task.taskDescription}
+            boardId={boardId}
+            id={task.taskId}
+            index={index}
+          />
+        </div>
+      ))}
+      <ActionButton boardId={boardId} listId={list.listId} />
+    </div>
+  );
+};
+
+export default List;
