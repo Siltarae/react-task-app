51e0c1e554736057d4348b5590cc2dd615e4bf7d
Siltarae <sjkwang1@gmail.com>
2024-06-28 15:52:06 +0900

    firebase.ts 파일 공개

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9sf2mEowN_uAPhsUWMUvYQ97YwvOS4rE",
+  authDomain: "react-test-app-98e14.firebaseapp.com",
+  projectId: "react-test-app-98e14",
+  storageBucket: "react-test-app-98e14.appspot.com",
+  messagingSenderId: "785118250613",
+  appId: "1:785118250613:web:126e404e282df14b659a3c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
